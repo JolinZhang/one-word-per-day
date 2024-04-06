7ddce57136ce470b806ab19623a49d37f6400863
Ru Zhang <ruzhang@Rus-Laptop.qis>
2024-04-06 00:07:47 -0500

    implementation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import OpenAI from 'openai'
 import express from 'express'
 import cors from 'cors'
+import 'dotenv/config'
 
 const PORT = 8000
 const openai = new OpenAI();
@@ -8,6 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.listen(PORT,() => console.log('your server is running on port' + PORT))
+
 app.post('/images', async (req, res) => {
   try{
     console.log(req.body.message)
@@ -18,23 +20,52 @@ app.post('/images', async (req, res) => {
     });
     console.log(response.data)
     res.send(response.data)
+    // const data = [
+    //   {
+    //     revised_prompt: 'Create an image that showcases a vibrant garden in full bloom. Dominating the center of the garden should be a large, captivating flower with an explosion of colors, its petals spreading wide open, revealing a bright, golden core. Beside it, make sure to add a mixture of smaller flowers in a riot of colors - red, yellow, orange, pink, and purple. Please also depict the sun above, casting a warm glow over the scene. Include the details such as dewdrops clinging to the petals and leaves, enriching the visual portrayal of this lively garden.',
+    //     url: 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-XnKg87NBohpu8mSTuHnt5JWC/user-i5cpF7rItwTBeunO3rkcswC4/img-DndLlljAlguZWXsak3jhL4kJ.png?st=2024-04-06T03%3A55%3A51Z&se=2024-04-06T05%3A55%3A51Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-04-05T23%3A44%3A00Z&ske=2024-04-06T23%3A44%3A00Z&sks=b&skv=2021-08-06&sig=m/kepvIPVExudOp4LVUx216YWo90Sh8XJ7g21/6%2BdSg%3D'
+    //   }
+    // ]
+    //res.send(data)
   }catch(error){
     console.error(error)
   }
 })
 
-app.post('/word', async (req, res) => {
+app.post('/word',  async (req, res) => {
   try{
-    console.log(req.body.message)
-    const response = await openai.images.generate
-    ({ 
-      model: "dall-e-3", 
-      prompt: req.body.message, 
-    });
-    console.log(response.data)
-    res.send(response.data)
+    //Radom pick a word from the list
+    const random = Math.floor(Math.random()*wordList.length)
+    const data = wordList[random]
+    console.log(data)
+    res.send(data)
   }catch(error){
     console.error(error)
   }
 })
 
+app.post('/wordDefinition', async (req, res) => {
+  try{
+    console.log("word definition "+ req.body.message)
+    const learnerKey = process.env.LEARNER_API_KEY
+    var params = new URLSearchParams({
+      key: learnerKey
+    })
+    const getWordBaseUrl = 'https://dictionaryapi.com/api/v3/references/learners/json/'+req.body.message +'?' + params
+    console.log(getWordBaseUrl)
+    const option = {
+      method: 'get',
+      headers:{
+        'Content-Type' : 'application/json'
+      }
+    }
+    const response = await fetch(getWordBaseUrl, option)
+    const data = await response.json()
+    console.log(data)
+    res.send(data)
+  }catch(eror){
+    console.error(error)
+  }
+})
+
+const wordList = ['apple', 'farmer', 'party', 'baby', 'father', 'picture', 'back', 'feet', 'pig', 'ball', 'fire', 'rabbit', 'bear', 'fish', 'rain', 'bed', 'floor', 'ring', 'bell', 'flower', 'robin', 'bird', 'game', 'Santa Claus', 'birthday', 'garden', 'school', 'boat', 'girl', 'seed', 'box', 'good-bye', 'sheep', 'boy', 'grass', 'shoe', 'bread', 'ground', 'sister', 'brother', 'hand', 'snow', 'cake', 'head', 'song', 'car', 'hill', 'squirrel', 'cat', 'home', 'stick', 'chair', 'horse', 'street', 'chicken', 'house', 'sun', 'children', 'kitty', 'table', 'Christmas', 'leg', 'thing', 'coat', 'letter', 'time', 'corn', 'man', 'top', 'cow', 'men', 'toy', 'day', 'milk', 'tree', 'dog', 'money', 'watch', 'doll', 'morning', 'water', 'door', 'mother', 'way', 'duck', 'name', 'wind', 'egg', 'nest', 'window', 'eye', 'night', 'wood', 'farm', 'paper'];
--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import './App.css';
-import { Tooltip } from 'react-tooltip'
-
-const SurpriseMe = () =>{}
+import './App.css'
+import React, {useState} from 'react'
 
 const App = () => {
   const [images, setImage] = useState(null)
   const [value, setValue] = useState("")
+  const [definition, setDefinition] = useState("")
+  const [audio, setAudio] = useState("")
+  const [audioUrl, setAudioUrl] = useState(null)
+
+  const surpriseMe = async() =>{
+    try{
+      const options = {
+        method: 'post',
+        headers: {
+          'Content-type':'application/json'
+        }
+      }
+      const response = await fetch('http://localhost:8000/word', options)
+      const data = await response.text()
+      console.log(data)
+      setValue(data)
+    }catch(error){
+      console.error(error)
+    }
+  }
+  const getWordDetail = async() =>{
+    try{
+      if(!value) return
+      const options = {
+        method: 'post',
+        headers:{
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+          message: value
+        })
+      }
+      const response = await fetch('http://localhost:8000/wordDefinition', options)
+      const data =  await response.json()
+      console.log(data)
+      setDefinition(data[0].shortdef)
+      setAudio(data[0].hwi.prs[0].sound.audio)
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  const playAudio = () => {
+     if(audio === "") return
+      const subdirectory = audio.charAt(0)
+      const getSoundBaseUrl = 'https://media.merriam-webster.com/audio/prons/en/us/mp3/'+subdirectory+'/'+audio+'.mp3'
+      let audioUrl = new Audio(getSoundBaseUrl)
+      setAudioUrl(audioUrl)
+      console.log(getSoundBaseUrl) 
+      if(audio.length === 0 || audioUrl === null || audioUrl === undefined) return
+      audioUrl.play()
+  } 
+ 
   const getImages = async()=> {
     if(!value) return
     try{
@@ -20,30 +71,38 @@ const App = () => {
         }
         const response = await fetch('http://localhost:8000/images', options)
         const data = await response.json()
+      
       console.log(data)
       setImage(data)
       }catch(error){
         console.error(error)
       }
   }
-  console.log(value)
   return (
     <div className="app">
       <section className="search-section">
       <p>Put your words here 
-        <span className="surprise" onlick={SurpriseMe}>Surprise me!</span>
+        <span className="surprise" onClick={surpriseMe}>Surprise me!</span>
       </p>
       <div className="input-container">
         <input value = {value} placeholder="a red apple" onChange={e => setValue(e.target.value)}/>
-        <button onClick={getImages}>Generate</button>
+        <button onClick={getWordDetail}>WordDetails</button>
+      </div>
+      </section>
+      <section className="word-definition">
+      <p>{definition}</p> 
+      {audio.length > 0 && <button onClick = {playAudio}>Sound</button>}
+      </section>
+      <section>
+      <div className="input-container">
+        <input value = {value} placeholder="a red apple" onChange={e => setValue(e.target.value)}/>
+        <button onClick={getImages}>GenerateImg</button> 
       </div>
       </section>
       <section className="image-section">
         {images?.map((image, _index) => 
-            <img data-tooltip-id="my-tooltip" data-tooltip-content={image.revised_prompt} data-tooltip-place="bottom-start"
-            key={_index} src={image.url} alt={image.revised_prompt}/>  
-        )} 
-      <Tooltip id="my-tooltip"/>
+            <img key={_index} src={image.url} alt={image.revised_prompt}/>  
+        )}
       </section>
       
     </div>
